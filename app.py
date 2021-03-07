a15725181ed3d83241e9a3eb0d40f4277a5011bd
Jamshid <mamatovjamshid506@gmail.com>
2021-03-07 21:27:53 +0500

    information about daily weather

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template
-from flask_sqlalchemy import SQLAlchemy,request
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
 import os
 import requests
 import json
@@ -19,7 +19,27 @@ def day_weather():
     content="daily weather"
     return render_template("day.html",content=content)
 
-
+@app.route("/day_weather",methods=['POST'])
+def get_day_city():
+    r=request.form
+    city=r.get("city")
+    # print(r.get("city"))
+    url=f"https://api.openweathermap.org/data/2.5/weather"
+    payload={
+        "q":city,
+        "appid":key
+    }
+    r=requests.get(url,payload)
+    data=r.json()
+    # print(data)
+    inform ="city not found"
+    if r.status_code==200:
+        name=data.get("name")
+        speed=data['wind']['speed']
+        temp=int(data["main"]["temp"]-273.15)
+        description=data['weather'][0]['description']
+        inform=f"city: {name} <br> wind speed: {speed} <br> temprature: {temp} <br> description:{description}<br> <a href='/'>go to weather home</a>"
+    return inform
 
 if __name__=="__main__":
     app.run(debug=True)
