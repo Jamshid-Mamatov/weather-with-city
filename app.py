f12bcdab8ad1204453a15228867ec615777844aa
Jamshid <mamatovjamshid506@gmail.com>
2021-03-07 20:27:41 +0500

    weather home

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy,request
+import os
+import requests
+import json
+
+
+key=os.environ['key']
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    content="weather home"
+    return render_template("home.html",content=content)
+
+if __name__=="__main__":
+    app.run(debug=True)
