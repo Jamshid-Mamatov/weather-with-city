f6ece5f9bb64e1cbb0aacd675c5bc75e1101c024
Jamshid Mamatov <76895281+Jamshid-Mamatov@users.noreply.github.com>
2021-03-08 15:16:37 +0500

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-tday=datetime.date(2021,3,3)
-print(tday.year,tday.month)
-print(tday.isoweekday())
-tdelta=datetime.timedelta(weeks=1)
-print(tday-tdelta)
-print(datetime.date())
